3845ad12c80ac26ad72c9a9c7d67c3d9d4344ee7
Sebastian Song <sebsong@berkeley.edu>
2021-05-11 22:11:42 -0700

    new lines and fix group import file

--- include/sebgine/Entity.hpp
+++ include/sebgine/Entity.hpp
@@ -2,7 +2,10 @@
 #define SEBGINE_UTILS_HPP
 
 #include <sebgine/entity/Entity.hpp>
-#include <sebgine/entity/VisualEntity.hpp>
-#include <sebgine/entity/ControllableEntity.hpp>
+#include <sebgine/entity/PlayerEntity.hpp>
+#include <sebgine/entity/NonPlayerEntity.hpp>
+
+#include <sebgine/entity/interfaces/Viewable.hpp>
+#include <sebgine/entity/interfaces/Controllable.hpp>
 
 #endif
